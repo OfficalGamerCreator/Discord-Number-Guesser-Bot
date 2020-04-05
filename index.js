0df826f74686e6553628153577a297eb0257d179
GamerCreator1 <58714825+GamerCreator1@users.noreply.github.com>
2020-04-05 17:40:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,45 +3,34 @@ const bot = new Discord.Client();
 
 var guesses;
 var num = 0;
+var channel;
 
 var info = require('./info.json');
 
 
 bot.on('message', (message) => {
     var mes = message.content.split(" ");
-    if(message.content == '!pick') {
-        message.reply('Picking a random number between 1 and 100');
-        num = Math.floor((Math.random() * 100) + 1);
+    if(message.content == '!setnumber') {
+        message.reply('Do !setnumber <channel> <number>');
+        num = mes[2];
+        channel = mes[1];
         guesses = 0;
+        message.reply('Channel set up completely' + channel + 'number is' num);
     }
-    if(mes[0] == '!guess') {
-        if (num == 0)
-        {
-            message.reply('Picking a random number between 1 and 100');
-            num = Math.floor((Math.random() * 100) + 1);
-            guesses = 0;
-        }
-        else if(mes[1] == num)
+       if(mes[0] == num)
         {
             guesses++;
             message.reply('You got it! Only took ' + guesses + ' tries.');
-            message.reply('Picking a random number between 1 and 100');
-            num = Math.floor((Math.random() * 100) + 1);
-            guesses = 0;
         }
-        else if(mes[1] < num)
+        else if(mes[0] < num)
         {
-            message.reply(mes[1] + ' is too low');
             guesses++;
         }
-        else if(mes[1] > num)
+        else if(mes[0] > num)
         {
-            message.reply(mes[1] + ' is too high');
             guesses++;
         }
-    }
-
-});
+    });
 
 
 
